bc0eafdf07bf93fbf5f59c05d86c15ec0881a1d5
Michael Sumner <mdsumner@gmail.com>
2018-02-26 20:35:25 +1100

    docs

---
+++ R/biseg.R
@@ -0,0 +1,11 @@
+## with triangles we already have implicit segments
+## so we want to find those, keeping the triangle grouping
+## and bisect each one as a simple subdivision for greater detail
+
+
+bisegment <- function(idx, xy) {
+  mat <- matrix(idx, nrow = 3)
+  x <- xy[mat[,1], ]
+  ## rah rah
+}
+
